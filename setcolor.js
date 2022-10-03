c2e6e904874d5e208554db63072fd86e065179f8
softacoder <113185323+softacoder@users.noreply.github.com>
2022-10-03 14:18:40 +0100

    Add files via upload

---
+++ setcolor.js
@@ -0,0 +1,9 @@
+let element_banana = document.getElementById('banana');
+element_banana.style.color = '#ffff00';
+element_banana.style.backgroundColor = '#666666';
+let element_apple = document.getElementById('apple');
+element_apple.style.color = '#33ff33';
+element_apple.style.backgroundColor = '#666666';
+let element_strawberry = document.getElementById('strawberry');
+element_strawberry.style.color = '#ff0000';
+element_strawberry.style.backgroundColor = '#666666';
